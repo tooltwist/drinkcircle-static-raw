34c74972e89162e2e226f627f2daba4f7d791b2c
philcal <philip.callender@tooltwist.com>
2016-12-27 23:37:16 +0800

    Incremental compilation of gulp files. Changed 'gulp serve' to only install changed pug files, and added 'gulp serve-all' to compile all changed pug files whenever any of them changes, for when layouts are changed. Running with 'gulp main serve' works for most purposes.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp');
+var debug = require('gulp-debug');
 var clean = require('gulp-clean');
 var pug = require('gulp-pug');
+var changed = require('gulp-changed');
 var sass = require('gulp-sass');
+var gutil = require('gulp-util');
 var browserSync   = require('browser-sync').create();
 var autoprefixer = require('gulp-autoprefixer');
 var exec = require('child_process').exec;
@@ -10,9 +13,10 @@ var root = '../ttstatic.github.io/'; // always add slash('/') at the end
 var dir = 'drinkcircle'; // project folder
 
 var paths = {
-  pug: ['./*.pug', '!**[^_]/*.pug'],
-  scss: 'assets/css/**/*.scss',
-  js: 'assets/scripts/**/*.js'
+    // This must be in the right order, so linked files are processed first.
+    pug: ['includes/**/*.pug', 'layout/**/*.pug', './*.pug', '!**/_*.pug', '!node_modules/**/*'],
+    scss: 'assets/css/**/*.scss',
+    js: 'assets/scripts/**/*.js'
 }
 
 // - ###########################################################################
@@ -25,38 +29,78 @@ gulp.task('default', ['clean'], function(cb) {
         cb(err);
     });
 });
-gulp.task('main', ['pug', 'sass', 'js', 'copy', 'bower']);
+gulp.task('main', ['pug-all', 'sass', 'js', 'copy', 'bower']);
 
 // - ###########################################################################
 // - Compile PUG files to HTML
 // - ###########################################################################
-gulp.task('pug', function() {
+gulp.task('pug-all', function() {
     /*
-     * Compile all pug files except files with
-     * file names that starts with an underscore('_').
-     */
+    * Compile ALL pug files except files with
+    * file names that starts with an underscore('_').
+    */
     return gulp.src(paths.pug)
-      .pipe(pug({
-          doctype: 'html',
-          pretty: true
-      }))
-      .pipe(gulp.dest(root + dir));
-      // .pipe(browserSync.stream());
+    // Convert the pug files to html files
+    .pipe(pug({
+        doctype: 'html',
+        pretty: true
+    }))
+    // Handle ug errors nicely
+    .on('error', function(err){
+        gutil.log(err.message);
+        this.emit('end'); // prevents watch from dying
+    })
+    // Write a message
+    .pipe(debug({title:'Installed'}))
+    // Place the resultant HTML file into the destination directory.
+    .pipe(gulp.dest(root + dir))
+    // Refresh the page in the browser
+    .pipe(browserSync.stream());
 });
-gulp.task('pug-watch', ['pug'], function (done) {
-    browserSync.reload();
+
+gulp.task('pug-incremental', (done) => {
+
+    var dest = root + dir;
+    gulp.src(paths.pug)
+    // Look for files newer than the corresponding .html
+    // file in the destination directory.
+    .pipe(changed(dest, {extension: '.html'}))
+    // Convert the pug files to html files
+    .pipe(pug({
+        doctype: 'html',
+        pretty: true
+    }))
+    // Handle ug errors nicely
+    .on('error', function(err){
+        gutil.log(err.message);
+        this.emit('end'); // prevents watch from dying
+    })
+    // Write a message
+    .pipe(debug({title:'Installed'}))
+    // Place the resultant HTML file into the destination directory.
+    .pipe(gulp.dest(dest))
+    // Refresh the page in the browser
+    .pipe(browserSync.stream());
+
+
     done();
 });
+
+
+gulp.task('watch', () => {
+    gulp.watch(paths.pug,['pug-incremental']);
+});
+
 
 // - ###########################################################################
 // - Compile JS files
 // - ###########################################################################
 gulp.task('js', function() {
     return gulp.src(paths.js)
-      // .pipe(browserify())
-      // .pipe(uglify())
-      .pipe(gulp.dest(root + dir + '/assets/scripts'))
-      .pipe(browserSync.stream());;
+    // .pipe(browserify())
+    // .pipe(uglify())
+    .pipe(gulp.dest(root + dir + '/assets/scripts'))
+    .pipe(browserSync.stream());;
 });
 
 // - ###########################################################################
@@ -64,10 +108,10 @@ gulp.task('js', function() {
 // - ###########################################################################
 gulp.task('sass', function() {
     return gulp.src(paths.scss)
-      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-      .pipe(autoprefixer())
-      .pipe(gulp.dest(root + dir + '/assets/css'))
-      .pipe(browserSync.stream());
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(root + dir + '/assets/css'))
+    .pipe(browserSync.stream());
 });
 
 // - ###########################################################################
@@ -84,7 +128,7 @@ var assets = [
 ];
 gulp.task('copy', function() {
     gulp.src(assets, { base: './'})
-        .pipe(gulp.dest(root + dir));
+    .pipe(gulp.dest(root + dir));
 });
 
 // - ###########################################################################
@@ -108,7 +152,7 @@ var bower = [
 ];
 gulp.task('bower', function() {
     gulp.src(bower, { base: './'})
-        .pipe(gulp.dest(root + dir));
+    .pipe(gulp.dest(root + dir));
 });
 
 // - ###########################################################################
@@ -116,21 +160,50 @@ gulp.task('bower', function() {
 // - ###########################################################################
 gulp.task('clean', function() {
     return gulp.src(root + dir, { read: false })
-        .pipe(clean({force: true})); // added the 'force' option because the
-                                     // directory is outside the gulpfile.js's
-                                     // root folder
+    .pipe(clean({force: true})); // added the 'force' option because the
+    // directory is outside the gulpfile.js's
+    // root folder
 });
 
 // - ###########################################################################
 // - Serve app and watch
 // - ###########################################################################
 gulp.task('serve', function() {
-  browserSync.init({
-    server: {
-      baseDir: root + dir
-    }
-  });
-  gulp.watch(paths.scss, ['sass']);
-  gulp.watch(paths.js, ['js']);
-  gulp.watch('./**/*.pug',['pug-watch']);
+    browserSync.init({
+        server: {
+            baseDir: root + dir
+        },
+        port: 3030,
+        //reloadDelay: 200 // Give the server time to pick up the new files.
+    });
+    console.log('')
+    console.log('Please note that "serve" compiles and installs changed')
+    console.log('pugs, but not other pugs that are dependant upon them.')
+    console.log('To force compile every pug, use "gulp serve-all".');
+    console.log('')
+    console.log('')
+    gulp.watch(paths.scss, ['sass']);
+    gulp.watch(paths.js, ['js']);
+    //    gulp.watch('./**/*.pug',['pug-watch']);
+    gulp.watch(paths.pug,['pug-incremental']);
+});
+
+gulp.task('serve-all', function() {
+    browserSync.init({
+        server: {
+            baseDir: root + dir
+        },
+        port: 3030,
+        //reloadDelay: 200 // Give the server time to pick up the new files.
+    });
+    console.log('')
+    console.log('Please note that "serve-all" compiles and installs')
+    console.log('ALL pugs, so is quite slow. To only compile changed')
+    console.log('pugs, use "gulp serve".');
+    console.log('')
+    console.log('')
+    gulp.watch(paths.scss, ['sass']);
+    gulp.watch(paths.js, ['js']);
+    //    gulp.watch('./**/*.pug',['pug-watch']);
+    gulp.watch(paths.pug,['pug-all']);
 });
