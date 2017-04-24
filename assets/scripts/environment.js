78ad51dad3901d72eff9e63eb3e2e997a5275e66
philcal <philip.callender@tooltwist.com>
2017-04-24 20:54:10 +0800

    [CONFIG] environment files

--- assets/scripts/environment.js
+++ assets/scripts/environment.js
@@ -4,46 +4,51 @@ var STORE_ID = 6;
 
 // Authservice-related
 if (LOCAL_DEPLOYENT) {
+
+  // Local Authservice
   var AUTHSERVICE_HOST = 'localhost';
   var AUTHSERVICE_PORT = 9090;
+  var AUTHSERVICE_TENANT = 'nodeclient';
+  var AUTHSERVICE_USE_DUMMY_LOGIN = false;
 
-} else {
-  var AUTHSERVICE_HOST = 'dcdev.authservice.io';
-  // var AUTHSERVICE_PORT = 443;
-  var AUTHSERVICE_PORT = 80;
-}
-var AUTHSERVICE_TENANT = 'nodeclient';
-var AUTHSERVICE_USE_DUMMY_LOGIN = false;
-
-
-
-// Crowdhound-related
-if (LOCAL_DEPLOYENT) {
+  // Local Crowdhound
   var CROWDHOUND_HOST = 'localhost';
   var CROWDHOUND_PORT = 4000;
-} else {
-  //var CROWDHOUND_HOST = 'ttcf-ch-alb-ch4dc-8064752.ap-southeast-1.elb.amazonaws.com';
-  var CROWDHOUND_HOST = 'dcdev.crowdhound.io';
-  //var CROWDHOUND_PORT = 443;
-  var CROWDHOUND_PORT = 80;
-}
-var CROWDHOUND_VERSION = "2.0";
-var CROWDHOUND_TENANT = 'drinkpoint';
+  var CROWDHOUND_VERSION = "2.0";
+  var CROWDHOUND_TENANT = 'drinkpoint';
 
-
-
-// Teaservice-related
-if (LOCAL_DEPLOYENT) {
+  // Local TEAservice
   var TEASERVICE_HOST = 'localhost';
   var TEASERVICE_PORT = 3000;
+  var TEASERVICE_ACCESS_TOKEN = '0613952f81da9b3d0c9e4e5fab123437';
+  var TEASERVICE_VERSION = '2.0.0';
+
 } else {
-  //var TEASERVICE_HOST = 'ttcf-tea-alb-tea4dc-841762890.ap-southeast-1.elb.amazonaws.com';
-  var TEASERVICE_HOST = 'dcdev.teaservice.io';
-  //var TEASERVICE_PORT = 443;
+  var REMOTE_PREFIX = 'dcdev';
+  var REMOTE_PREFIX = 'dcmvp';
+  var REMOTE_PREFIX = 'ratdev';
+
+  // Authservice
+  var AUTHSERVICE_HOST = REMOTE_PREFIX + '.authservice.io';
+  var AUTHSERVICE_PORT = 80;
+  var AUTHSERVICE_TENANT = 'nodeclient';
+  var AUTHSERVICE_USE_DUMMY_LOGIN = false;
+
+  // Crowdhound
+  var CROWDHOUND_HOST = REMOTE_PREFIX + '.crowdhound.io';
+  var CROWDHOUND_PORT = 80;
+  var CROWDHOUND_VERSION = "2.0";
+  var CROWDHOUND_TENANT = 'drinkpoint';
+
+  // TEAservice
+  var TEASERVICE_HOST = REMOTE_PREFIX + '.teaservice.io';
   var TEASERVICE_PORT = 80;
+  var TEASERVICE_ACCESS_TOKEN = '0613952f81da9b3d0c9e4e5fab123437';
+  var TEASERVICE_VERSION = '2.0.0';
 }
-var TEASERVICE_ACCESS_TOKEN = '0613952f81da9b3d0c9e4e5fab123437';
-var TEASERVICE_VERSION = '2.0.0';
+
+// Image handling variables
+const CLOUDINARY_CLOUD_NAME = 'twist-resources';
 
 
 // A few system-wide constants
